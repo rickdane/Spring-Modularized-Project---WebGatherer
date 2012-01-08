df7c0a243b55dd877d0c0e7c4d6d15004bbd00f3
Rick Dane <rick_developer@gmx.com>
2012-01-08 15:23:12 -0700

    refining AJAX form submission, fully implmeneting login system

--- src/main/java/com/rickdane/springmodularizedproject/web/UserController.java
+++ src/main/java/com/rickdane/springmodularizedproject/web/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.encoding.MessageDigestPasswor
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -39,4 +40,10 @@ public class UserController {
         user.persist();
         return "redirect:/users/" + encodeUrlPathSegment(user.getId().toString(), request);
     }
+    
+    @RequestMapping(value = "/loggedIn", produces = "text/html")
+    public String loggedIn() {
+        return "loggedIn";
+    }
+    
 }
